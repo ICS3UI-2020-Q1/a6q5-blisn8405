25ef0e1fe467cbf4fbba3986956f4f77ab8cbaa2
blisn8405 <blisn8405@wrdsb.ca>
2020-10-08 15:51:12 +0000

    q5

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+    // crete scanner for user input
+    Scanner input= new Scanner(System.in);
+final int INTEGERS = 2;
+int number1 = 0;
+int number2 = 0;
+    //create an array with 10 spots
+    double[] user = new double[INTEGERS];
+//put integers into the array using a loop
+ System.out.println("Please enter the 2 values for the array");
+for(int i =0; i < INTEGERS; i++){
+    user[i] = input.nextDouble();
+}
+
+// user a for loop to go through the array and find the number
+
+if(user[0] > user[1]){
+   System.out.println("Your numbers in ascending order are " + user[1] + " and " + user[0]);
+} else 
+if(user[0] < user[1]){
+  System.out.println("Your numbers in ascending order are " + user[0] + " and " + user[1]);
+  
+}
+}
 }
